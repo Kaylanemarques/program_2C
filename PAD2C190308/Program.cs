7911c968c9248e13ee2c545aa0f3f8654a62afba
kaylane <kaylanemarquesok@gmail.com>
2019-03-15 11:35:07 -0300

    projeto iniciado

---
+++ PAD2C190308/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PAD2C190308
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("projeto");
+
+        }
+    }
+}
